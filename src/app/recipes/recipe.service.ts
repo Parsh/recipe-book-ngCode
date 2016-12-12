0cc23bd905abe7c6b80870c02f1937a220d0b553
Parsh Jain <parsh.cosmos11@gmail.com>
2016-12-13 03:24:15 +0530

    Http Integrated with Firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable , EventEmitter} from '@angular/core';
+import { Headers , Http , Response} from '@angular/http'
+import { Observable , Subscription} from 'rxjs/Rx'
+import 'rxjs/Rx'
+
 import { Recipe} from './recipe';
 import { Ingredients } from '../shared/ingredients'
 
 @Injectable()
 export class RecipeService {
+
+  recipesChanged = new EventEmitter<Recipe[]>()
+
   private recipes: Recipe[] = [
     new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg',[
         new Ingredients( 'FrenchFries',2),
@@ -13,7 +20,7 @@ export class RecipeService {
   ];
 
 
-  constructor() { }
+  constructor( private http : Http) { }
 
   getRecipes(){
     return this.recipes;
@@ -33,6 +40,25 @@ export class RecipeService {
 
   editRecipe( oldRecipe : Recipe , newRecipe : Recipe){
    this.recipes[this.recipes.indexOf(oldRecipe)]= newRecipe
+  }
+
+  storeData(){
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    })
+    return this.http.put('https://recipebook-9c17d.firebaseio.com/recipes.json',body,{headers:headers})
+  }
+
+  fetchData(){
+     return this.http.get('https://recipebook-9c17d.firebaseio.com/recipes.json')
+     .map((response:Response)=> response.json())
+     .subscribe(
+            (data:Recipe[]) => {
+              this.recipes = data
+              this.recipesChanged.emit(this.recipes)
+            }
+        )
   }
 
 }

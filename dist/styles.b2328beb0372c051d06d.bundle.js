83ceace962089491f7363bf571ea0718431aa86f
Parsh Jain <parsh.cosmos11@gmail.com>
2016-12-16 16:51:08 +0530

    Deployement Issue fixed

--- dist/styles.b2328beb0372c051d06d.bundle.js
+++ dist/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,4],{415:function(n,o){},958:function(n,o,c){n.exports=c(415)}},[958]);
+webpackJsonp([2,4],{349:function(n,o){},861:function(n,o,c){n.exports=c(349)}},[861]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
